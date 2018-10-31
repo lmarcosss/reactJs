a962e3fa77d8b57bd08b4c29abae8e59161e0670
Leonardo dos Santos Marcos <leonardo.marcos@cwi.com.br>
2018-10-31 16:47:14 -0300

    feat: iniciando projeto react-redux-firebase ;

---
+++ react-redux-firebase/src/config/dev.js
@@ -0,0 +1,5 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyB76qTcOB2gpUP3uv4E1iXwmjv45rCSDt4",
+  authDomain: "my-notes-ff6df.firebaseapp.com",
+  databaseURL: "https://my-notes-ff6df.firebaseio.com"
+};
